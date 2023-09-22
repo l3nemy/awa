5729236dbf3a6631c837e9a10bb6312f96db4d89
l3nemy <leejihyeok1267@gmail.com>
2023-09-22 15:05:33 +0900

    Chore: Run cargo fmt

--- src/platform_specific/windows.rs
+++ src/platform_specific/windows.rs
@@ -18,7 +18,7 @@ unsafe extern "system" fn enum_window(hwnd: HWND, lparam: isize) -> i32 {
     let p = FindWindowExA(hwnd, 0 as _, "SHELLDLL_DefView\0".as_ptr() as _, 0 as _);
     if p != 0 as _ {
         *workerw = FindWindowExA(0 as _, hwnd, "WorkerW\0".as_ptr() as _, 0 as _);
-    } 
+    }
     1
 }
 
@@ -27,12 +27,26 @@ pub(crate) fn set_desktop_window(window: &Window) {
     unsafe {
         let window_hwnd = window.hwnd() as HWND;
 
-        let progman = FindWindowExA(GetDesktopWindow(), 0 as _, "Progman\0".as_ptr() as _, "Program Manager\0".as_ptr() as _);
+        let progman = FindWindowExA(
+            GetDesktopWindow(),
+            0 as _,
+            "Progman\0".as_ptr() as _,
+            "Program Manager\0".as_ptr() as _,
+        );
 
         let mut result: usize = mem::zeroed();
         let result_ptr: *mut usize = &mut result;
 
-        if SendMessageTimeoutW(progman, 0x052C, 0 as _, 0 as _, SMTO_NORMAL, 10000, result_ptr) == 0 {
+        if SendMessageTimeoutW(
+            progman,
+            0x052C,
+            0 as _,
+            0 as _,
+            SMTO_NORMAL,
+            10000,
+            result_ptr,
+        ) == 0
+        {
             panic!("Error creating WorkerW: {:#?}", result);
         }
 
